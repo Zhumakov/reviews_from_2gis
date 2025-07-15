044fff7a7fc01730a0c0142da08444686dce465d
Zhumakov <zhumakov06@mail.ru>
2025-07-15 19:45:01 +0300

    Написан парсер, дополнены тесты.
    Написан парсер, он вытаскивает из исходной html страницы имя
    пользователя, текст отзыва и рейтинг. Тесты дополнены чтобы проверять
    дополнительно соответствие имени пользователя и рейтинга

--- source/parser.py
+++ source/parser.py
@@ -1,9 +1,104 @@
 """Модуль для парсинга html страниц 2GIS на отзывы."""
 
+from bs4 import BeautifulSoup, Tag
+
+
+USERNAME_CSS_SELECTOR = "div._1pi8bc0 > div._o7qbud > div._j1kt73 > div._m80g57y > span._14quei > \
+span._wrdavn > span._16s5yj36"
+
+# Варианты контейнера для отзыва и рейтинга
+REVIEW_CONTAINER_CSS_SELECTORS = ("div._1i94jn5", "div._1pi8bc0")
+REVIEW_CSS_SELECTOR = "div._49x36f > a"
+RATING_CSS_SELECTOR = "div._92aljh > div._1m0m6z5 > div._1fkin5c"
+
+
+class ParserError(Exception):
+    """Представляет собой ошибки парсинга страницы."""
+
 
 class ReviewesParser:
-    def __init__(self, html_content: str):
-        pass
+    def __init__(self, html_content: str) -> None:
+        """
+        Создаёт объект парсера с html кодом страницы.
 
-    def get_comments(self) -> list[dict[str, str]]:
-        pass
+        Args:
+            html_content: html код страницы
+
+        """
+        self.html_content = html_content
+
+    def get_rewiews(self) -> list[dict[str, str]]:
+        """
+        Парсит html страницу и вытаскивает из неё все отзывы в виде словаря.
+
+        Returns:
+            Поля:
+                username: имя пользователя
+                review: текст отзыва
+                rating: рейтинг, поставленный пользователем
+
+        Raises:
+            ParserError:
+                Ошибки при парсинге, связанные с неожиданной структурой
+
+        """
+        soup = BeautifulSoup(self.html_content, "lxml")
+        tab_navigator = soup.select_one("div._qvsf7z")
+
+        if tab_navigator is None:
+            err_msg = "На странице не найдена вкладка с отзывами"
+            raise ParserError(err_msg)
+
+        rewiews_tab = tab_navigator.find_parents("div")
+        if len(rewiews_tab) < 2:
+            err_msg = "Вкладка с отзывами изменила структуру"
+            raise ParserError(err_msg)
+
+        rewiews_container = rewiews_tab[1]
+        rewiews = rewiews_container.select("div._1k5soqfl")
+
+        list_rewiews: list[dict[str, str]] = []
+        for rewiew in rewiews:
+            rewiew_field = self.__parse_review_fields(rewiew)
+            list_rewiews.append(rewiew_field)
+
+        return list_rewiews
+
+    def __parse_review_fields(self, review_block: Tag) -> dict[str, str]:
+        """
+        Парсит блок отзыва, вытаскивая из него имя пользователя, текст отзыва и рейтинг.
+
+        Args:
+            review_block: блок отзыва
+
+        Returns:
+            Словарь с полями отзыва
+
+        Raises:
+            ParserError:
+                Ошибки при парсинге, связанные с неожиданной структурой
+
+        """
+        username = review_block.select_one(USERNAME_CSS_SELECTOR)
+        if username is None:
+            msg_error = "Не удалось найти имя пользователя"
+            raise ParserError(msg_error)
+
+        for review_container in REVIEW_CONTAINER_CSS_SELECTORS:
+            review = review_block.select_one(f"{review_container} > {REVIEW_CSS_SELECTOR}")
+            if review is not None:
+                break
+        else:
+            msg_error = "Не удалось найти текст отзыва"
+            raise ParserError(msg_error)
+
+        for review_container in REVIEW_CONTAINER_CSS_SELECTORS:
+            rating = review_block.select_one(f"{review_container} > {RATING_CSS_SELECTOR}")
+            if rating is not None:
+                rating = str(len(rating.select("span")))
+                break
+        else:
+            msg_error = "Не удалось найти оценку"
+            raise ParserError(msg_error)
+
+        return {"username": username.text, "review": review.text, "rating": rating}
--- tests/unit_tests/parser_tests.py
+++ tests/unit_tests/parser_tests.py
@@ -6,35 +6,46 @@ from source.parser import ReviewesParser
 
 
 @pytest.mark.parametrize(
-    ("plained_comment"),
+    ("plained_username", "plained_comment", "plained_rating"),
     [
-        "очень грязно, не убирают особо, картошка сухая и мелкая, бургер норм был, "
-        "ну как то вообще не очень, обходите стороной",
-        # -------SECOND-------
-        "Чисто и вкусно",
+        (
+            "кира ракова",
+            # -----REVIEW------
+            "очень грязно, не убирают особо, картошка сухая и мелкая, бургер норм был, "
+            "ну как то вообще не очень, обходите стороной",
+            # -----REVIEW------
+            "2",
+        ),
+        ("Емеля Емеля", "Чисто и вкусно", "5"),
     ],
 )
-async def test_review_parser(plained_comment: str, html_content: str) -> None:
+async def test_review_parser(
+    plained_username: str, plained_comment: str, plained_rating: str, html_content: str
+) -> None:
     """
     Тестирует парсер для отзывов.
 
     Args:
-        plained_comment: существующий на странице комментарий
+        plained_username: планируемое имя пользователя
+        plained_comment: планируемый текст отзыва
+        plained_rating: планируемый рейтинг
         html_content: html код страницы
 
     Raises:
         AssertionError: Ошибка парсера
 
     """
-    assert plained_comment in html_content, "Планируемого отзыва не найдено"
+    assert plained_username in html_content
+    assert plained_rating in html_content
     parser = ReviewesParser(html_content)
-    comments: list[dict[str, str]] = parser.get_comments()
+    comments: list[dict[str, str]] = parser.get_rewiews()
 
     assert comments, "Отзывы не были найдены"
 
     for comment in comments:
         if plained_comment in comment.get("review", ""):
-            assert True
+            assert plained_username in comment.get("username", "")
+            assert plained_rating in comment.get("rating", "")
             break
     else:
         raise AssertionError("Ожидаемого отзыва не найдено")
