2c8af35e2f21a15fe2ba04679cccf1cadd1b004c
Zhumakov <zhumakov06@mail.ru>
2025-07-17 10:44:17 +0300

    Упростил CSS селекторы в парсере.

--- source/parser.py
+++ source/parser.py
@@ -3,14 +3,11 @@
 from bs4 import BeautifulSoup, Tag
 
 
-USERNAME_CSS_SELECTOR = "div._1pi8bc0 > div._o7qbud > div._j1kt73 > div._m80g57y > span._14quei > \
-span._wrdavn > span._16s5yj36"
-DATE_CSS_SELECTOR = "div._1pi8bc0 > div._o7qbud > div._j1kt73 > div._m80g57y > div._a5f6uz"
+USERNAME_CSS_SELECTOR = "span._16s5yj36"
+DATE_CSS_SELECTOR = "div._a5f6uz"
 
-# Варианты контейнера для отзыва и рейтинга
-REVIEW_CONTAINER_CSS_SELECTORS = ("div._1i94jn5", "div._1pi8bc0")
 REVIEW_CSS_SELECTOR = "div._49x36f > a"
-RATING_CSS_SELECTOR = "div._92aljh > div._1m0m6z5 > div._1fkin5c"
+RATING_CSS_SELECTOR = "div._1fkin5c"
 
 
 class ParserError(Exception):
@@ -47,25 +44,24 @@ class ReviewesParser:
         """
         soup = BeautifulSoup(self.html_content, "lxml")
         tab_navigator = soup.select_one("div._qvsf7z")
-
         if tab_navigator is None:
             err_msg = "На странице не найдена вкладка с отзывами"
             raise ParserError(err_msg)
 
-        rewiews_tab = tab_navigator.find_parents("div")
-        if len(rewiews_tab) < 2:
+        reviews_tab = tab_navigator.find_parents("div")
+        if len(reviews_tab) < 2:
             err_msg = "Вкладка с отзывами изменила структуру"
             raise ParserError(err_msg)
 
-        rewiews_container = rewiews_tab[1]
-        rewiews = rewiews_container.select("div._1k5soqfl")
+        reviews_container = reviews_tab[1]
+        reviews = reviews_container.select("div._1k5soqfl")
 
-        list_rewiews: list[dict[str, str]] = []
-        for rewiew in rewiews:
+        list_reviews: list[dict[str, str]] = []
+        for rewiew in reviews:
             rewiew_field = self.__parse_review_fields(rewiew)
-            list_rewiews.append(rewiew_field)
+            list_reviews.append(rewiew_field)
 
-        return list_rewiews
+        return list_reviews
 
     def __parse_review_fields(self, review_block: Tag) -> dict[str, str]:
         """
@@ -92,22 +88,16 @@ class ReviewesParser:
             msg_error = "Не удалось найти дату отзыва"
             raise ParserError(msg_error)
 
-        for review_container in REVIEW_CONTAINER_CSS_SELECTORS:
-            review = review_block.select_one(f"{review_container} > {REVIEW_CSS_SELECTOR}")
-            if review is not None:
-                break
-        else:
+        review = review_block.select_one(REVIEW_CSS_SELECTOR)
+        if review is None:
             msg_error = "Не удалось найти текст отзыва"
             raise ParserError(msg_error)
 
-        for review_container in REVIEW_CONTAINER_CSS_SELECTORS:
-            rating = review_block.select_one(f"{review_container} > {RATING_CSS_SELECTOR}")
-            if rating is not None:
-                rating = str(len(rating.select("span")))
-                break
-        else:
+        rating = review_block.select_one(RATING_CSS_SELECTOR)
+        if rating is None:
             msg_error = "Не удалось найти оценку"
             raise ParserError(msg_error)
+        rating = str(len(rating.select("span")))
 
         return {
             "username": username.text,
