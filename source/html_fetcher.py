660565fb7d82fc8348be2883581bd90f1f8055ee
Zhumakov <zhumakov06@mail.ru>
2025-07-17 15:17:35 +0300

    Сменил библиотеку для получения html страницы.
    Переписал html_fetcher с использованием playwright вместо selenium.
    Значитально увеличилась скорость получения страниц.

--- source/html_fetcher.py
+++ source/html_fetcher.py
@@ -1,9 +1,6 @@
 import asyncio
 
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
-from selenium_stealth import stealth
+from playwright.async_api import async_playwright
 
 
 TAB_NAVIGATOR_CSS_SELECTOR = "div._qvsf7z"
@@ -13,13 +10,12 @@ NUMBER_OF_REVIEWS_CSS_SELECTOR = "span._1xhlznaa"
 LOADING_WAIT = 2
 
 
+class FetcherError(Exception):
+    """Ошибки при получении страницы с отзывами."""
+
+
 class HtmlFetcher:
     """Получает содержимое html страницы."""
-
-    chrome_options = Options()
-    chrome_options.add_argument("--headless=new")
-    chrome_options.add_argument("--disable-gpu")
-    chrome_options.add_argument("--no-sandbox")
 
     def __init__(self, url: str) -> None:
         """Используется для получения html содержимого страницы."""
@@ -37,36 +33,43 @@ class HtmlFetcher:
         Returns:
             html страницы
 
-        """
-        driver = webdriver.Chrome(options=self.chrome_options)
-        stealth(
-            driver,
-            languages=["ru-RU", "ru"],
-            platform="Linux x86_64",
-            webgl_vendor="Intel Inc.",
-            renderer="Mesa DRI Interl(R) UHD Graphics 620 (Kabylake GT2)",
-            fix_hairline=True,
-        )
-        try:
-            driver.get(self.url)
-            await asyncio.sleep(LOADING_WAIT + 2)
+        Raises:
+            FetcherError: ошибка при получении страницы
 
-            tab_navigator = driver.find_element(By.CSS_SELECTOR, TAB_NAVIGATOR_CSS_SELECTOR)
-            number_of_reviews = int(
-                tab_navigator.find_element(By.CSS_SELECTOR, NUMBER_OF_REVIEWS_CSS_SELECTOR).text
+        """
+        async with async_playwright() as p:
+            browser = await p.chromium.launch(headless=True)
+            context = await browser.new_context(
+                locale="ru-RU",
+                user_agent=(
+                    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36"
+                    "(KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
+                ),
             )
+            page = await context.new_page()
+            await page.goto(self.url)
+
+            tab_navigator = page.locator(TAB_NAVIGATOR_CSS_SELECTOR)
+            await tab_navigator.wait_for()
+
+            reviews_count_text = await tab_navigator.locator(
+                NUMBER_OF_REVIEWS_CSS_SELECTOR
+            ).text_content()
+            if reviews_count_text is None:
+                err_msg = "Не найден контейнер с вкладками"
+                raise FetcherError(err_msg)
+
+            number_of_reviews = int(reviews_count_text.replace(" ", "").replace(" ", ""))
 
             number_of_cycles = round((number_of_reviews - last_saved_comment) / 50)
             for _ in range(number_of_cycles):
-                driver.execute_script(
-                    "arguments[0].scrollIntoView()",
-                    tab_navigator.find_elements(
-                        By.CSS_SELECTOR,
-                        REVIEW_CSS_SELECTOR,
-                    )[-1],
-                )
+                review_containers = page.locator(REVIEW_CSS_SELECTOR)
+                count = await review_containers.count()
+
+                last_review = review_containers.nth(count - 1)
+                await last_review.scroll_into_view_if_needed()
                 await asyncio.sleep(LOADING_WAIT)
 
-            return driver.page_source
-        finally:
-            driver.quit()
+            content = await page.content()
+            await browser.close()
+            return content
